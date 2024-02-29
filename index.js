8e612f6cebd76cf5dabb5b7de0619e1908b0fa12
supersuryaansh <prod.genx@gmail.com>
2024-02-29 12:57:06 +0530

    Reverted index.js and created test

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
+// Importing required modules
 const HyperDHT = require('hyperdht')  // HyperDHT module for DHT functionality
 const net = require('net')  // Node.js net module for creating network clients and servers
 const libNet = require('@hyper-cmd/lib-net')  // Custom network library
 const goodbye = require('graceful-goodbye')  // Graceful shutdown library
+const connPiper = libNet.connPiper  // Custom connection piper function
 const libKeys = require('@hyper-cmd/lib-keys') // To generate a random preSeed for server seed.
 
-// Generate a new preSeed and seed every time the module is required
-function generateKeys() {
-  const preSeed = libKeys.randomBytes(32).toString('hex')
-  const seed = Buffer.from(preSeed, 'hex')
-  const keyPair = HyperDHT.keyPair(seed)
-  return { preSeed, seed, keyPair }
-}
+const preSeed = libKeys.randomBytes(32).toString('hex')
 
+const seed = Buffer.from(preSeed, 'hex')
+const dht = new HyperDHT()
+const keyPair = HyperDHT.keyPair(seed)
 const stats = {}
 
 //start the client on port and the address specified
 function serve(port, address) {
-  const { preSeed, seed, keyPair } = generateKeys()
-
-  console.log(preSeed);
-
   const server = dht.createServer({
     reusableSocket: true
   }, c => {
@@ -33,16 +28,12 @@ function serve(port, address) {
 
   server.listen(keyPair).then(() => {
     //do whatever you want after starting to listen
-    console.log(keyPair.publicKey.toString('hex'))
   })
 }
-
 goodbye(async () => {
   await dht.destroy()
 })
-
 //export the function
 module.exports.serve = serve;
-
 //export the public key
 module.exports.pubKey = keyPair.publicKey.toString('hex');
---
+++ test.js
@@ -0,0 +1,4 @@
+const server = require('./index.js');
+server.serve(3400,'127.0.0.1')
+
+console.log(server.pubKey)
