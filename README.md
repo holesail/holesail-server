f446692b5cdc6b12a18f20893b781fc227a59275
supersuryaansh <prod.genx@gmail.com>
2024-02-29 12:50:05 +0530

    Added readme file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # holesail-server
+Create and announce your server on the HyperDHT, to be used with other Holesail modules.
+
+## Installation
+```shell
+npm i holesail-server --save-dev
+```
+## Usage
+```js
+const server = require(holesail-server)
+server.serve(5000,'127.0.0.1')
+```
--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-// Importing required modules
 const HyperDHT = require('hyperdht')  // HyperDHT module for DHT functionality
 const net = require('net')  // Node.js net module for creating network clients and servers
 const libNet = require('@hyper-cmd/lib-net')  // Custom network library
 const goodbye = require('graceful-goodbye')  // Graceful shutdown library
-const connPiper = libNet.connPiper  // Custom connection piper function
 const libKeys = require('@hyper-cmd/lib-keys') // To generate a random preSeed for server seed.
 
-const preSeed = libKeys.randomBytes(32).toString('hex')
+// Generate a new preSeed and seed every time the module is required
+function generateKeys() {
+  const preSeed = libKeys.randomBytes(32).toString('hex')
+  const seed = Buffer.from(preSeed, 'hex')
+  const keyPair = HyperDHT.keyPair(seed)
+  return { preSeed, seed, keyPair }
+}
 
-const seed = Buffer.from(preSeed, 'hex')
-const dht = new HyperDHT()
-const keyPair = HyperDHT.keyPair(seed)
 const stats = {}
 
 //start the client on port and the address specified
 function serve(port, address) {
+  const { preSeed, seed, keyPair } = generateKeys()
+
+  console.log(preSeed);
+
   const server = dht.createServer({
     reusableSocket: true
   }, c => {
@@ -28,12 +33,16 @@ function serve(port, address) {
 
   server.listen(keyPair).then(() => {
     //do whatever you want after starting to listen
+    console.log(keyPair.publicKey.toString('hex'))
   })
 }
+
 goodbye(async () => {
   await dht.destroy()
 })
+
 //export the function
 module.exports.serve = serve;
+
 //export the public key
 module.exports.pubKey = keyPair.publicKey.toString('hex');
