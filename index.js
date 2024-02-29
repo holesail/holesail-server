ba372690c82417fe510bad7ec23ec82afc695818
supersuryaansh <prod.genx@gmail.com>
2024-02-29 14:20:44 +0530

    Turned module in a class for starting multiple server within same script

--- index.js
+++ index.js
@@ -3,37 +3,47 @@ const HyperDHT = require('hyperdht')  // HyperDHT module for DHT functionality
 const net = require('net')  // Node.js net module for creating network clients and servers
 const libNet = require('@hyper-cmd/lib-net')  // Custom network library
 const goodbye = require('graceful-goodbye')  // Graceful shutdown library
-const connPiper = libNet.connPiper  // Custom connection piper function
 const libKeys = require('@hyper-cmd/lib-keys') // To generate a random preSeed for server seed.
 
-const preSeed = libKeys.randomBytes(32).toString('hex')
+class holesailServer{
+  constructor () {
+    this.dht = new HyperDHT();
+    this.stats = {};
+    this.server = null;
+  }
 
-const seed = Buffer.from(preSeed, 'hex')
-const dht = new HyperDHT()
-const keyPair = HyperDHT.keyPair(seed)
-const stats = {}
+  keyPair(){
+    let preSeed = libKeys.randomBytes(32).toString('hex')
+    let seed = Buffer.from(preSeed, 'hex')
+    //the keypair here is not a reference to the function above
+    return HyperDHT.keyPair(seed);
+  }
 
-//start the client on port and the address specified
-function serve(port, address) {
-  const server = dht.createServer({
-    reusableSocket: true
-  }, c => {
-    // Connection handling using custom connection piper function
-    connPiper(c, () => {
-      return net.connect(
-        { port: +port, host: address, allowHalfOpen: true }
-      )
-    }, { isServer: true, compress: false }, stats)
-  })
+  //start the client on port and the address specified
+  serve(port, address) {
+     this.server =this. dht.createServer({
+      reusableSocket: true
+    }, c => {
+      // Connection handling using custom connection piper function
+      libNet.connPiper(c, () => {
+        return net.connect(
+          { port: +port, host: address, allowHalfOpen: true }
+        )
+      }, { isServer: true, compress: false }, this.stats)
+    })
 
-  server.listen(keyPair).then(() => {
-    //do whatever you want after starting to listen
-  })
-}
-goodbye(async () => {
-  await dht.destroy()
-})
-//export the function
-module.exports.serve = serve;
-//export the public key
-module.exports.pubKey = keyPair.publicKey.toString('hex');
+    this.server.listen(this.keyPair()).then(() => {
+      //do whatever you want after starting to listen
+    })
+  }
+
+  async shutdown(){
+    await this.dht.destroy();
+  }
+
+  getPublicKey(){
+    return this.keyPair().publicKey.toString('hex');
+  }
+} //end server Class
+
+module.exports = holesailServer;
--- test.js
+++ test.js
@@ -1,4 +1,19 @@
-const server = require('./index.js');
-server.serve(3400,'127.0.0.1')
+const DHT = require('./index.js');  // Assuming the DHTServer class is in a file named DHTServer.js
 
-console.log(server.pubKey)
+// Create an instance of the DHTServer
+const server =  new DHT();
+const server2 = new DHT()
+
+// Start the DHT server
+server.serve(5000, '127.0.0.1');
+server2.serve(5100, '127.0.0.1');
+
+// Get the public key of the server
+console.log('Server public key:', server.getPublicKey());
+
+console.log('Server public key:', server2.getPublicKey());
+// Graceful shutdown
+process.on('SIGINT', async () => {
+  await server.shutdown();
+  process.exit(0);
+});
