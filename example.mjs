d83512b9e43ab782dc56bde1d5be8820a6c0af65
supersuryaansh <prod.genx@gmail.com>
2025-02-22 22:41:52 +0530

    put records on DHT

--- example.mjs
+++ example.mjs
@@ -7,7 +7,7 @@ await server1.serve(
     port: 9001,
     host: '0.0.0.0',
     udp: false,
-    seed: 'c8233747d4b1aa259d3cfedd1b20364e35972c8e3af1d39b6041f5cea4f4a9d9',
+    seed: '88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589',
     secure: true
   },
   async () => {
